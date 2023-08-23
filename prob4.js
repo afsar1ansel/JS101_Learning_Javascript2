746858ee5b856a3208f026acccc36b3a67bb90f0
md afsar <mdafsaransal@gmail.com>
2023-08-23 22:48:38 +0530

    pushing the assingments

---
+++ prob4.js
@@ -0,0 +1,22 @@
+let report = "𝓡𝓔𝓟𝓞𝓡𝓣 𝓒𝓐𝓡𝓓";
+
+let name = "Raju Sharma";
+let school = "D.A.V Public School";
+let grade = "12th";
+let section = "A";
+let rollno = 70;
+
+let submath = 88;
+let subphysics = 79;
+let subchemistry = 64;
+
+console.log(report)
+console.log("Name-> ",name)
+console.log("School-> ",school)
+console.log("Grade-> ",grade)
+console.log("Section-> ",section)
+console.log("Rollno-> ",rollno)
+console.log("Math-> ",submath)
+console.log("Physics-> ",subphysics)
+console.log("Chemistry-> ",subchemistry)
+          
